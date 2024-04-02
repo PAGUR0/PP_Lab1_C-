ee3e4601210eb232831162c2a94a4a5ccc37b17d
Ivan Kosaev <119807994+PAGUR0@users.noreply.github.com>
2024-04-02 15:55:55 +0300

    Добавьте файлы проекта.

---
+++ PP_Lab2_C#/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число x (x!=0): ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число шагов  n (n > 0): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Abs(x) > 1 && n > 0)
+{
+    double result = 0;
+    for(int i = 0; i < n; i++)
+    {
+        result += 1 / ((2 * i + 1) * Math.Pow(x, 2 * i + 1));
+    }
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Вы ввели не корректные данные");
+}
+
